02d3d8cc448b2265863922c62fac3f9a3ca0cac2
truongly2003 <118749801+truongly2003@users.noreply.github.com>
2023-10-21 21:55:58 +0700

    update

--- js/search.js
+++ js/search.js
@@ -1,14 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const search = document.querySelector('#search-icon');
-    const searchForm = document.querySelector('#search-form');
-    const close = document.querySelector('#close')
-    const searchBox = document.querySelector('#search-box')
-      search.onclick = () => {
-        searchForm.classList.toggle('active');
-        searchBox.focus();
-      }
-      close.onclick=() => {
-        searchForm.classList.toggle('active');
-      }
-  });
-  
+const search = {
+    handleEvent: function () {
+        const search = document.querySelector('#search-icon')
+        const searchForm = document.querySelector('#search-form')
+        const close = document.querySelector('#close')
+        const searchBox = document.querySelector('#search-box')
+        search.onclick = () => {
+            searchForm.classList.toggle('active');
+            searchBox.focus();
+        }
+        close.onclick = () => {
+            searchForm.classList.toggle('active');
+        }
+    },
+    start: function () {
+        this.handleEvent()
+    }
+}
+export default search;
+
+
+
