ad9900a48793a75282aa242034424d3598b96ab1
truongly2003 <118749801+truongly2003@users.noreply.github.com>
2023-12-05 01:07:47 +0700

    update

--- js/product-list.js
+++ js/product-list.js
@@ -161,7 +161,7 @@ const products = [
   },
   {
     id: 21,
-    name: 'Giỏ Trái Cây Thăm Tặng',
+    name: 'Giỏ Trái Cây ',
     price: 12000,
     quantity: 14,
     img: './image/product-cart/CT01-CTT-500k.jpg',
@@ -169,7 +169,7 @@ const products = [
   },
   {
     id: 22,
-    name: 'Giỏ Trái Cây Gia Đình',
+    name: 'Giỏ Trái Cây',
     price: 9000,
     quantity: 20,
     img: './image/product-cart/gio-trai-cay-579.jpg',
@@ -193,7 +193,7 @@ const products = [
   },
   {
     id: 25,
-    name: 'Hộp Trái Cây Quà Tặng',
+    name: 'Hộp Quà Tặng',
     price: 12000,
     quantity: 15,
     img: './image/product-cart/hop-trai-cay-qua-tang.jpg',
@@ -201,7 +201,7 @@ const products = [
   },
   {
     id: 26,
-    name: 'Hộp Nho Không Hạt',
+    name: 'Nho Không Hạt',
     price: 18000,
     quantity: 12,
     img: './image/product-cart/hop-trai-cay-qua-tang-nho-xanh-khong-hat.jpg',
@@ -233,7 +233,7 @@ const products = [
   },
   {
     id: 30,
-    name: 'Giỏ Quà Sinh Nhật',
+    name: 'Quà Sinh Nhật',
     price: 14000,
     quantity: 14,
     img: './image/product-cart/sinh-nhat.jpg',
